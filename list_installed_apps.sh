dffa3695129f4b83b80f5ac7f8c7ae4857263c6d
Jean-Marc Meessen <jean-marc@meessen-web.org>
2022-01-13 18:17:34 +0100

    Start MAC CB 2

--- list_installed_apps.sh
+++ list_installed_apps.sh
@@ -3,5 +3,4 @@ set -e
 
 tree /Applications/ -d -L 1 -N -o app_list.txt
 tree /Library/ -d -L 2 -N -o lib_list.txt
-brew list -1 -l --versions >brew_list.txt
-brew cask list > brew-cask_list.txt
+brew list --versions >brew_list.txt
